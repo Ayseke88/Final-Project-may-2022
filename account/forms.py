541091723bcd0e850460ba6c1a1605b22ff0acd0
Ayseke88 <91011188+Ayseke88@users.noreply.github.com>
2022-05-22 02:28:04 +0600

    по новому

--- account/forms.py
+++ account/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from django.contrib.auth.forms import (AuthenticationForm, PasswordResetForm,
                                        SetPasswordForm)
 
-from .models import Customer, Address
+from .models import Customer
+
+from .models import  Address
 
 class UserAddressForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = ["full_name", "phone", "address_line", "address_line2", "town_city", "postcode"]
+        fields = ["full_name", "phone", "address_line", "town_city", "postcode"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,17 +20,12 @@ class UserAddressForm(forms.ModelForm):
         self.fields["address_line"].widget.attrs.update(
             {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
         )
-        self.fields["address_line2"].widget.attrs.update(
-            {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
-        )
         self.fields["town_city"].widget.attrs.update(
             {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
         )
         self.fields["postcode"].widget.attrs.update(
             {"class": "form-control mb-2 account-form", "placeholder": "Full Name"}
         )
-
-
 
 class UserLoginForm(AuthenticationForm):
 
@@ -88,49 +85,3 @@ class RegistrationForm(forms.ModelForm):
         self.fields['password2'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Repeat Password'})
 
-
-class PwdResetForm(PasswordResetForm):
-
-    email = forms.EmailField(max_length=254, widget=forms.TextInput(
-        attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        u = Customer.objects.filter(email=email)
-        if not u:
-            raise forms.ValidationError(
-                'Unfortunatley we can not find that email address')
-        return email
-
-
-class PwdResetConfirmForm(SetPasswordForm):
-    new_password1 = forms.CharField(
-        label='New password', widget=forms.PasswordInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
-    new_password2 = forms.CharField(
-        label='Repeat password', widget=forms.PasswordInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
-
-
-class UserEditForm(forms.ModelForm):
-
-    email = forms.EmailField(
-        label='Account email (can not be changed)', max_length=200, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'email', 'id': 'form-email', 'readonly': 'readonly'}))
-
-    user_name = forms.CharField(
-        label='Firstname', min_length=4, max_length=50, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'form-firstname', 'readonly': 'readonly'}))
-
-    first_name = forms.CharField(
-        label='Username', min_length=4, max_length=50, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Firstname', 'id': 'form-lastname'}))
-
-    class Meta:
-        model = Customer
-        fields = ('email', 'user_name', 'first_name',)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['user_name'].required = True
-        self.fields['email'].required = True
