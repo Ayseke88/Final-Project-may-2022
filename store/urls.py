cf56acbe58db5bb8e013fdb92797ef822be4436c
Abjik <200107009@stu.sdu.edu.kz>
2022-05-23 11:56:47 +0600

    ЯЯ СДЕЛАЛ

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.product_all, name='store_home'),
-    path('', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('<slug:slug>', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
