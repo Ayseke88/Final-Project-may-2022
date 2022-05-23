d90c881158db5250b784d8371b8813181c5b6e43
Abjik <200107009@stu.sdu.edu.kz>
2022-05-23 12:12:37 +0600

    +back to site

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,7 @@ app_name = 'store'
 urlpatterns = [
     path('', views.product_all, name='store_home'),
     path('about/', views.about, name='about'),
+    path('', views.index, name='index'),
     path('<slug:slug>', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
